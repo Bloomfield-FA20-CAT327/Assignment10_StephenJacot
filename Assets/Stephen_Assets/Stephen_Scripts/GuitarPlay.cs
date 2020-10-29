c5772cf06751bd6235dc91f24c3f3f4eefdd3b6d
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-10-29 14:35:14 -0400

    Final Push

---
+++ Assets/Stephen_Assets/Stephen_Scripts/GuitarPlay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RedColorDetector : MonoBehaviour
+{
+    GameControllerScript gc;
+    private AudioSource source;
+    public AudioClip guitar;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var cuberenderer = this.gameObject.GetComponent<Renderer>();
+        GameControllerScript gc = GameObject.FindWithTag("GameController").GetComponent<GameControllerScript>();
+        if(other.gameObject.tag == "Player")
+        {
+            this.gameObject.GetComponent<Renderer>().material.SetColor("Red", Color.red);
+
+            if(cuberenderer.material.HasProperty("Red"))
+            {
+                source.PlayOneShot(guitar);
+            }
+            
+        }
+
+        
+        
+        
+    }
+}
---
+++ Assets/Stephen_Assets/Stephen_Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadFirstGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
